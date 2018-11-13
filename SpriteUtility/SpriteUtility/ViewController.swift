4432c8538b953f641ac37600e122a9cd88474e69
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-13 12:08:51 -0700

    Add sprite kit scene and crab animations.

--- SpriteUtility/SpriteUtility/ViewController.swift
+++ SpriteUtility/SpriteUtility/ViewController.swift
@@ -1,6 +1,18 @@
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    var skscene: CustomScene? = nil
+    
+    @IBOutlet weak var skview: SKView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
